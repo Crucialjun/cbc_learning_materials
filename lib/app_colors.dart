690ac1468b2806a8ea3e4ca6fac96516a773d4d1
Crucial Jun <nich.otieno@gmail.com>
2022-02-13 20:08:13 +0300

    onboarding_screen

---
+++ lib/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/// App Colors Class - Resource class for storing app level color constants
+class AppColors {
+  static const Color primaryColor = Color(0xFF1565c0);
+  static const Color PRIMARY_COLOR_LIGHT = Color.fromARGB(255, 54, 154, 236);
+  static const Color PRIMARY_COLOR_DARK = Color(0xFF0D3656);
+  static const Color ACCENT_COLOR = Color(0xFFF2DA04);
+}
--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,10 +1,52 @@
+import 'package:cbc_learning_materials/app_colors.dart';
+import 'package:cbc_learning_materials/global_consts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
 
   @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Hello Worls"));
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                appName,
+                style: TextStyle(
+                    fontSize: 18,
+                    color: AppColors.primaryColor,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Skip",
+                style: TextStyle(color: AppColors.PRIMARY_COLOR_LIGHT),
+              )
+            ],
+          )
+        ]),
+      ),
+    );
   }
 }
